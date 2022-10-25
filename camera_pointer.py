0274617b1b91d24dffd8b8c8f73328928edde4a5
Rahul Singh <75153249+rahul-singh01@users.noreply.github.com>
2022-10-25 18:50:33 +0530

    Update camera_pointer.py

--- camera_pointer.py
+++ camera_pointer.py
@@ -10,3 +10,11 @@ while True:
     output = face_mesh.process(rgb_frame)
     landmarks_points = output.multi_face_landmarks
     frame_h , frame_w , _= frame.shape
+    if(landmarks_points):
+        landmarks = landmarks_points[0].landmark
+        for landmark in landmarks:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame , (x,y), 1 , (0,255,0))
+    cv2.imshow("Eye Controlled Mouse" , frame)
+    cv2.waitKey(1)
